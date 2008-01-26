dcf444df422cdd4cf7a7da85a7217cb7972d5317
Choe Hwanjin <choe.hwanjin@gmail.com>
2008-01-27 03:09:55 +0900

    CJK unified 한자 사용:  * hanja.txt 파일의 compatibility 한자를 모두 unified hanja로 바꿈    앞으로는 hanja.txt는 모두 unified hanja로만 작성할 것임  * merge.py에서 compatibility 한자를 찾아내어 unified로 바꾸는 코드를 추가
    git-svn-id: http://kldp.net/svn/hangul/libhangul/trunk@139 8f00fcd2-89fc-0310-932e-b01be5b65e01

--- data/hanja/merge.py
+++ data/hanja/merge.py
@@ -4,24 +4,39 @@ import sys
 
 header = []
 
-# load freq table
+compat_table = {}
 freq_table = {}
 
-def load_frequency(filename):
-    try:
-	src = open(filename, 'r')
-	for i in src.readlines():
-	    list = i.strip().decode('utf-8').split(u':')
-	    key = list[0]
-	    value = float(list[1])
-	    if freq_table.has_key(key):
-		freq_table[key] = max(freq_table[key], value)
-	    else:
-		freq_table[key] = value
+def load_compat(filename):
+    src = open(filename, 'r')
+    for i in src.readlines():
+	list = i.strip().split('\t')
+	key = unichr(int(list[0], 16))
+	value = unichr(int(list[1], 16))
+	compat_table[key] = value
+    src.close()
 
-	src.close()
-    except:
-	return
+def get_unified(text):
+    res = u''
+    for i in text:
+	if compat_table.has_key(i):
+	    res += compat_table[i]
+	else:
+	    res += i
+    return res
+
+def load_frequency(filename):
+    src = open(filename, 'r')
+    for i in src.readlines():
+	list = i.strip().decode('utf-8').split(u':')
+	key = list[0]
+	value = float(list[1])
+	if freq_table.has_key(key):
+	    freq_table[key] = max(freq_table[key], value)
+	else:
+	    freq_table[key] = value
+
+    src.close()
 
 def get_frequency(key):
     if freq_table.has_key(key):
@@ -29,52 +44,53 @@ def get_frequency(key):
     else:
 	return 0
 
+# load freq table
 load_frequency('freq-hanja.txt')
 load_frequency('freq-hanjaeo.txt')
 
+# load compatibility char table
+load_compat('compat-table.txt')
+
 table = {}
 for file in sys.argv[1:]:
-    try:
-	src = open(file, 'r')
+    src = open(file, 'r')
 
-	for line in src.readlines():
-	    if line.startswith('#'):
-		header += line;
-		continue;
+    for line in src.readlines():
+	if line.startswith('#'):
+	    header += line;
+	    continue;
 
-	    line = line.strip().decode('utf-8').split(u':')
-	    if len(line) < 3:
-		continue
+	line = line.strip().decode('utf-8').split(u':')
+	if len(line) < 3:
+	    continue
 
-	    key = line[0]
-	    value = line[1]
-	    comment = line[2]
-	    freq = get_frequency(value)
+	key = line[0]
+	value = get_unified(line[1])
+	comment = line[2]
+	freq = get_frequency(value)
 
-	    if table.has_key(key):
-		isDuplicate = False
-		# check duplicate
-		for i in table[key]:
-		    if i['value'] == value and i['comment'] == comment:
-			isDuplicate = True
-			sys.stderr.write('%s:%s is duplicate\n' % (key.encode('utf-8'), value('utf-8')))
-			continue
-		    elif i['value'] == value and i['comment'] != comment:
-			sys.stderr.write('%s:%s has different comments\n' % (key.encode('utf-8'), value('utf-8')))
-			sys.stderr.write('\t%s\n' % (i['comment'].encode('utf-8')))
-			sys.stderr.write('\t%s\n' % (comment.encode('utf-8')))
-			isDuplicate = True
-			continue
+	if table.has_key(key):
+	    isDuplicate = False
+	    # check duplicate
+	    for i in table[key]:
+		if i['value'] == value and i['comment'] == comment:
+		    isDuplicate = True
+		    sys.stderr.write('%s:%s is duplicate\n' % (key.encode('utf-8'), value.encode('utf-8')))
+		    continue
+		elif i['value'] == value and i['comment'] != comment:
+		    sys.stderr.write('%s:%s has different comments\n' % (key.encode('utf-8'), value.encode('utf-8')))
+		    sys.stderr.write('\t%s\n' % (i['comment'].encode('utf-8')))
+		    sys.stderr.write('\t%s\n' % (comment.encode('utf-8')))
+		    isDuplicate = True
+		    continue
 
-		if not isDuplicate:
-		    table[key].append({ 'key' : key, 'value': value, 'freq': freq, 'comment': comment })
-	    else:
-		table[key] = [ { 'key': key, 'value': value, 'freq': freq, 'comment': comment } ]
+	    if not isDuplicate:
+		table[key].append({ 'key' : key, 'value': value, 'freq': freq, 'comment': comment })
+	else:
+	    table[key] = [ { 'key': key, 'value': value, 'freq': freq, 'comment': comment } ]
 
-	src.close()
+    src.close()
 
-    except:
-	continue
 
 keys = table.keys()
 keys.sort()
