a60c0fae26f6715df90fdba998046f03e823978d
Choe Hwanjin <choe.hwanjin@gmail.com>
2008-02-02 16:06:06 +0900

    libhangul내부적으로 사용할 hangulinternals.h 추가
    git-svn-id: http://kldp.net/svn/hangul/libhangul/trunk@155 8f00fcd2-89fc-0310-932e-b01be5b65e01

---
+++ hangul/hangulinternals.h
@@ -0,0 +1,6 @@
+#ifndef libhangul_hangulinternals_h
+#define libhangul_hangulinternals_h
+
+#define N_ELEMENTS(array) (sizeof (array) / sizeof ((array)[0]))
+
+#endif /* libhangul_hangulinternals_h */
