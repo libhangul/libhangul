7d4686cd9b8faf15a525b84aad0441d0318c5b30
Choe Hwanjin <choe.hwanjin@gmail.com>
2008-05-03 14:05:59 +0900

    ChangeLog가 없을 경우 처리 libtoolize가 없는 경우 glibtoolize를 사용하도록 처리
    git-svn-id: http://kldp.net/svn/hangul/libhangul/trunk@180 8f00fcd2-89fc-0310-932e-b01be5b65e01

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-libtoolize --automake
+
+test -f ChangeLog || touch ChangeLog
+
+if libtoolize --version | grep GNU > /dev/null 2>&1 ; then
+    libtoolize=libtoolize
+else
+    libtoolize=glibtoolize
+fi
+
+$libtoolize --automake
 aclocal $ACLOCAL_AMFLAGS
 autoheader
 automake --add-missing
