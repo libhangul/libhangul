0b0c04f9878fc836725efd2d049292b3e5bb8054
Choe Hwanjin <choe.hwanjin@gmail.com>
2009-09-03 23:15:53 +0900

    불필요한 함수 선언 제거 참고: http://lists.kldp.net/pipermail/hangul-hackers/2009-September/000392.html
    git-svn-id: http://kldp.net/svn/hangul/libhangul/trunk@198 8f00fcd2-89fc-0310-932e-b01be5b65e01

--- hangul/hangul.h
+++ hangul/hangul.h
@@ -45,9 +45,6 @@ bool hangul_is_jaso(ucschar c);
 bool hangul_is_jamo(ucschar c);
 
 ucschar hangul_jaso_to_jamo(ucschar ch);
-ucschar hangul_choseong_to_jamo(ucschar ch);
-ucschar hangul_jungseong_to_jamo(ucschar ch);
-ucschar hangul_jongseong_to_jamo(ucschar ch);
 
 ucschar hangul_choseong_to_jongseong(ucschar ch);
 ucschar hangul_jongseong_to_choseong(ucschar ch);
@@ -137,9 +134,6 @@ HanjaList*   hanja_table_match_exact(const HanjaTable* table, const char *key);
 HanjaList*   hanja_table_match_prefix(const HanjaTable* table, const char *key);
 HanjaList*   hanja_table_match_suffix(const HanjaTable* table, const char *key);
 void         hanja_table_delete(HanjaTable *table);
-
-int          hanja_table_txt_to_bin(const char* txtfilename,
-				    const char* binfilename);
 
 int          hanja_list_get_size(const HanjaList *list);
 const char*  hanja_list_get_key(const HanjaList *list);
