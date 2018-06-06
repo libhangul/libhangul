20983f0c8de0798b2443f137152a83233e4154a8
Choe Hwanjin <choe.hwanjin@gmail.com>
2018-06-06 16:51:23 +0900

    Add copyright notice to hangulkeyboard.h

--- hangul/hangulkeyboard.h
+++ hangul/hangulkeyboard.h
@@ -1,3 +1,20 @@
+/* libhangul
+ * Copyright (C) 2018 Choe Hwanjin
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 static const ucschar hangul_keyboard_table_2[] = {
     0x0000,     /* 0x00 null                                         */
     0x0000,     /* 0x01 start of heading                             */
