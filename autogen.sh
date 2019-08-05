8b0ca3c80c5a0ca0e37bb009c9c257b3fbbbf179
Hodong Kim <cogniti@gmail.com>
2019-08-05 13:58:48 +0900

    Fixed compilation error

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,43 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 test -f ChangeLog || touch ChangeLog
+test -f config.rpath || touch config.rpath
 
-libtoolize=`which glibtoolize`
-if test -z "$libtoolize"; then
-    libtoolize=libtoolize
+olddir=`pwd`
+cd "$srcdir"
+
+mkdir -p m4
+
+PKGCONFIG=`which pkg-config`
+if test -z "$PKGCONFIG"; then
+    echo "pkg-config not found, please install pkg-config"
+    exit 1
 fi
 
-intltoolize --copy --force
-$libtoolize --automake --copy --force
-aclocal $ACLOCAL_AMFLAGS
-autoheader
-automake --add-missing --copy
-autoconf
+LIBTOOLIZE=`which libtoolize`
+if test -z $LIBTOOLIZE; then
+    echo "libtoolize not found, please install libtool package"
+    exit 1
+fi
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+    echo "intltoolize not found, please install intltool package"
+    exit 1
+else
+    intltoolize --force --copy --automake || exit $?
+fi
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+    echo "autoreconf not found, please install autoconf package"
+    exit 1
+else
+    autoreconf --force --install --verbose || exit $?
+fi
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
