61ad1965c00289935b876139ddb2d72625cb4758
Won-Kyu Park <wkpark@gmail.com>
2023-03-14 02:52:54 +0900

    bindings: update ruby binding

--- bindings/ruby/extconf.rb
+++ bindings/ruby/extconf.rb
@@ -1,7 +1,9 @@
 require 'mkmf'
 
 dir_config('hangul')
-have_library('hangul', 'hangul_ic_new')
+find_header('hangul.h', '../../hangul')
+have_library('expat')
+find_library('hangul', 'hangul_ic_new', '../../hangul/.libs')
 create_makefile('hangul')
 
 # vim: set sts=2 sw=2 et:
